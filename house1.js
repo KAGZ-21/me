e49e488d9d4785312ab3c68ad6af9daf61aca00c
KAGZ-21 <kagabojean2003@gmail.com>
2024-10-29 10:08:57 -0700

    Add files via upload

---
+++ house1.js
@@ -0,0 +1,25 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
